088c19f5d7e6c17059be3173559c69ff8e80ff99
MLazarevic36 <hrle96ab@gmail.com>
2018-03-31 19:04:50 +0200

    izrada drawera

---
+++ app/src/main/java/Fragments/FragmentContent.java
@@ -0,0 +1,52 @@
+package Fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.hrle.android_portal.R;
+
+
+public class FragmentContent extends Fragment {
+
+    private static final String KEY_TITLE="Content";
+
+    public FragmentContent() {
+        // Required empty public constructor
+    }
+
+
+    public static FragmentContent newInstance(String param1, String param2) {
+        FragmentContent fragment = new FragmentContent();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_fragment_content, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        String title = getArguments().getString(KEY_TITLE);
+        view.findViewById(R.id.tit)
+
+    }
+}
